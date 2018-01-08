6909687c200525e7d43cd2d8ea87315cd5036862
gerosyab <gerosyab@gmail.com>
2018-01-08 17:14:45 +0900

    Add assets

---
+++ app/src/main/java/net/gerosyab/guitaroid/GuitaroidApplication.java
@@ -0,0 +1,34 @@
+package net.gerosyab.guitaroid;
+
+import android.app.Application;
+import android.content.res.Configuration;
+
+/**
+ * Created by donghe on 2018-01-02.
+ */
+
+public class GuitaroidApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+
+}
